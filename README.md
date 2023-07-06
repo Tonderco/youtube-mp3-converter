ea6c4de66ffa9c62cd7b8d690f0c664daefad0ee
TomasOnderco <tomas.onderco@schlieger.cz>
2023-07-06 16:07:28 +0200

    make app

---
+++ README.md
@@ -0,0 +1,12 @@
+# YouTube to MP3 Converter
+
+## Description
+
+This is a simple YouTube to mp3 converter react app
+### Local run
+
+* Copy `.env.example` to `.env`
+* Put your own api key in `.env` file
+* `npm install`
+* `npm run dev`
+* Open `http://localhost:5173` in your browser
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,57 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import './App.css';
+import {useRef, useState} from "react";
+import axios from "axios";
+import {youtube_parser} from "./utils.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+   const inputUrlRef = useRef();
+   const [urlResult, setUrlResult] = useState(null);
+
+   const handleSubmit = (e) => {
+       e.preventDefault();
+       console.log(inputUrlRef.current.value);
+       const youtubeID = youtube_parser(inputUrlRef.current.value);
+       console.log(youtubeID);
+
+       const options = {
+           method: 'GET',
+           url: 'https://youtube-mp36.p.rapidapi.com/dl',
+           headers: {
+               'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+               'X-RapidAPI-Host': 'youtube-mp36.p.rapidapi.com'
+           },
+           params: {
+               id: youtubeID,
+           }
+       }
+       axios(options)
+           .then(res => setUrlResult(res.data.link))
+           .catch(err => console.log(err))
+
+       inputUrlRef.current.value = "";
+   }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank" rel="noreferrer">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="app">
+        <span className="logo">youtube2mp3</span>
+        <section className="content">
+            <h1 className="content_title">
+                YouTube to MP3 Converter
+            </h1>
+            <p className="content_description">
+                Convert and download youtube videos to mp3 (audio) or mp4 (video) files for free. There is no registration or software needed.
+            </p>
+
+            <form onSubmit={handleSubmit} className="form">
+                <input ref={inputUrlRef} className="form_input" type="text" placeholder="Paste your YouTube URL here" />
+                <button className="form_button" type="submit">Convert</button>
+            </form>
+            {urlResult ?
+                <a target='_blank' rel='noreferrer' href={urlResult} className="download_btn">Download MP3</a> : ''
+            }
+
+        </section>
+    </div>
   )
 }
 
